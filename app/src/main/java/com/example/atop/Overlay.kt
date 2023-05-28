f799794bed9e416b019f9df0ce8ec1dd9779acf3
Chandradeep Chowdhury <chandradeep.choudhury@gmail.com>
2023-05-28 14:36:51 -0700

    basic overlay working

--- app/src/main/java/com/example/atop/Overlay.kt
+++ app/src/main/java/com/example/atop/Overlay.kt
@@ -9,6 +9,11 @@ import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.view.WindowManager
+import android.os.Handler
+import android.os.HandlerThread
+import android.os.Looper
+import android.os.Process
+import android.widget.TextView
 
 class Overlay: Service() {
 
@@ -16,6 +21,20 @@ class Overlay: Service() {
     private lateinit var overlayLayoutParams: WindowManager.LayoutParams
     private var layoutType: Int? =null
     private lateinit var windowManager: WindowManager
+
+    //Performance metrics
+    private lateinit var monitoringHandlerThread: HandlerThread
+    private lateinit var monitoringHandler: Handler
+    private lateinit var monitoringRunnable: Runnable
+    private val mainHandler = Handler(Looper.getMainLooper())
+
+    private lateinit var gpuNumberTextView: TextView
+    private lateinit var powerNumberTextView: TextView
+    private lateinit var cpuNumberTextView: TextView
+    private lateinit var memoryNumberTextView: TextView
+    private lateinit var fpsNumberTextView: TextView
+    private lateinit var fps1LowNumberTextView: TextView
+    private lateinit var fps01LowNumberTextView: TextView
 
     override fun onBind(p0: Intent?): IBinder? {
         return null
@@ -44,6 +63,57 @@ class Overlay: Service() {
         overlayLayoutParams.x = 0
         overlayLayoutParams.y = 0
 
+        //Monitoring thread init
+        monitoringHandlerThread = HandlerThread("MonitoringThread", Process.THREAD_PRIORITY_BACKGROUND)
+        monitoringHandlerThread.start()
+        monitoringHandler = Handler(monitoringHandlerThread.looper)
+        monitoringRunnable = Runnable { monitorPerformance() }
+
+        //TextView init
+        gpuNumberTextView = overlayView.findViewById(R.id.gpuNumber)
+        powerNumberTextView = overlayView.findViewById(R.id.powerNumber)
+        cpuNumberTextView = overlayView.findViewById(R.id.cpuNumber)
+        memoryNumberTextView = overlayView.findViewById(R.id.memoryNumber)
+        fpsNumberTextView = overlayView.findViewById(R.id.fpsNumber)
+        fps1LowNumberTextView = overlayView.findViewById(R.id.fps1LowNumber)
+        fps01LowNumberTextView = overlayView.findViewById(R.id.fps01LowNumber)
+
+        monitoringHandler.post(monitoringRunnable)
+
         windowManager.addView(overlayView,overlayLayoutParams)
+    }
+
+    private fun monitorPerformance() {
+        //Logic to calculate metrics
+        val gpuUtilization =  0
+        val powerConsumption = 0
+        val cpuUtilization = 0
+        val memoryUtilization = 0
+        val currentFps = 0
+        val fps1Low = 0
+        val fps01Low = 0
+
+        //Update TextViews
+        mainHandler.post {
+            gpuNumberTextView.text = gpuUtilization.toString()
+            powerNumberTextView.text = powerConsumption.toString()
+            cpuNumberTextView.text = cpuUtilization.toString()
+            memoryNumberTextView.text = memoryUtilization.toString()
+            fpsNumberTextView.text = currentFps.toString()
+            fps1LowNumberTextView.text = fps1Low.toString()
+            fps01LowNumberTextView.text = fps01Low.toString()
+        }
+
+        //Update Interval: 1s
+        monitoringHandler.postDelayed(monitoringRunnable, 1000)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        windowManager.removeView(overlayView)
+
+        //Quit the thread
+        monitoringHandler.removeCallbacks(monitoringRunnable)
+        monitoringHandlerThread.quitSafely()
     }
 }
